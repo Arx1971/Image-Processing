ca13912f10864b4d0ef947da797eaffad1475096
Arx1971 <adnan.rahin13@gmail.com>
2019-10-04 01:18:37 -0400

    Histogram Model

--- Image-Histogram/__init__.py
+++ Image-Histogram/__init__.py
@@ -1,1 +1,6 @@
-print("git init>>>")
+import numpy as np
+import matplot as mp
+import cv2 as cv
+
+underexposed_data = cv.imread("Overexposure-Sample.jpg")
+overexposed_data = cv.imread("Overexposure-Sample.jpg")
