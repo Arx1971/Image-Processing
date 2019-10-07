bfba47e20393a30120a46d6653c2ce31f67b3659
Arx1971 <adnan.rahin13@gmail.com>
2019-10-07 01:08:10 -0400

    Histogram Plot

--- Image-Histogram/__init__.py
+++ Image-Histogram/__init__.py
@@ -5,13 +5,13 @@ import statistics
 
 
 def calculate_histogram(source, bands):
-    row, col = under_exposed.shape[0:2]
+    row, col = source.shape[0:2]
     histogram = np.zeros(256)
     for i in range(row):
         for j in range(col):
             vector = source[i, j]
             if bands == 3:
-                average = (int(vector[0])+int(vector[1])+int(vector[2]))/3
+                average = (int(vector[0]) + int(vector[1]) + int(vector[2])) / 3
                 histogram[int(average)] += 1
             else:
                 histogram[vector[bands]] += 1
@@ -19,5 +19,17 @@ def calculate_histogram(source, bands):
     return np.array(histogram)
 
 
-under_exposed = cv2.imread("underexpose.jpg")
+def plot_histogram(img_matrix):
+    color = ('r', 'g', 'b', 'k')
+    for i in range(0, len(color)):
+        hist = calculate_histogram(img_matrix, i)
+        plt.plot(hist, color=color[i])
 
+    plt.show()
+
+
+under_exposed = cv2.imread("underexpose.jpg")
+plot_histogram(under_exposed)
+
+over_exposed = cv2.imread("overexpose.jpg")
+plot_histogram(over_exposed)
