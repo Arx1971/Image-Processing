b9199d697547e0cf5d05604d45360d579eaf1fe4
Arx1971 <adnan.rahin13@gmail.com>
2019-10-07 00:46:09 -0400

    First Commit

--- Image-Histogram/__init__.py
+++ Image-Histogram/__init__.py
@@ -1,6 +1,23 @@
 import numpy as np
-import matplot as mp
-import cv2 as cv
+import matplotlib.pyplot as plt
+import cv2
+import statistics
 
-underexposed_data = cv.imread("Overexposure-Sample.jpg")
-overexposed_data = cv.imread("Overexposure-Sample.jpg")
+
+def calculate_histogram(source, bands):
+    row, col = under_exposed.shape[0:2]
+    histogram = np.zeros(256)
+    for i in range(row):
+        for j in range(col):
+            vector = source[i, j]
+            if bands == 3:
+                average = (int(vector[0])+int(vector[1])+int(vector[2]))/3
+                histogram[int(average)] += 1
+            else:
+                histogram[vector[bands]] += 1
+
+    return np.array(histogram)
+
+
+under_exposed = cv2.imread("underexpose.jpg")
+
