fda127682b24c705015dcc63e26a80aef13de8ab
Arx1971 <adnan.rahin13@gmail.com>
2019-10-07 21:12:39 -0400

    Error 102 is processing

--- Image-Histogram/__init__.py
+++ Image-Histogram/__init__.py
@@ -32,8 +32,13 @@ def plot_histogram(img_matrix, plot_title):
     img.show()
 
 
-under_exposed = cv2.imread("underexpose.jpg")
-plot_histogram(under_exposed, "underexposed_plot")
+# Problem 1
+# under_exposed = cv2.imread("underexpose.jpg")
+# plot_histogram(under_exposed, "underexposed_plot")
+# over_exposed = cv2.imread("overexpose.jpg")
+# plot_histogram(over_exposed, "overexposed_plot")
 
-over_exposed = cv2.imread("overexpose.jpg")
-plot_histogram(over_exposed, "overexposed_plot")
+# Problem 2
+uniform_scene = cv2.imread("uniform_scene.jpg")
+nonuniform_scene = cv2.imread("nonuniform_scene.jpg")
+
